360340948d031d7ef629454ed0c1ff4a531de293
Gia Hy Nguyen <giahy03@outlook.com>
2025-02-14 13:04:52 +0100

    Add empty Login and Register pages

---
+++ app/register.tsx
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Colors } from "@/constants/Colors"; // Make sure Colors are defined
+
+export default function Register() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Register</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: Colors.background,
+    },
+    title: {
+        fontSize: 40,
+        paddingTop: 30,
+        color: Colors.text,
+        fontWeight: "bold",
+    },
+});
