fc512348d1f646ca623885de47c65080b5cd7d8a
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-02-20 10:19:59 +0100

    Add a button and logic for initiating BankID web login

--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,29 @@
-import { Text, View, StyleSheet } from "react-native";
+import {Text, View, StyleSheet, TouchableOpacity} from "react-native";
+import { useAuth0 } from "react-native-auth0";
 import { Colors } from "@/constants/Colors"; // Make sure Colors are defined
 
 export default function Login() {
+    const { authorize } = useAuth0();
+
+    const onLoginPress = async () => {
+        try {
+            await authorize();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Login</Text>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => onLoginPress()}
+            >
+                <View style={styles.buttonContent}>
+                    <Text style={styles.buttonText}>Login with BankID</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -20,5 +39,25 @@ const styles = StyleSheet.create({
         paddingTop: 30,
         color: Colors.text,
         fontWeight: "bold",
+    },
+    button: {
+        width: "90%",
+        backgroundColor: Colors.button,
+        paddingVertical: 10,
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "flex-start",
+    },
+    buttonContent: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        paddingHorizontal: 15,
+        gap: 10,
+    },
+    buttonText: {
+        color: Colors.lightpink,
+        fontSize: 30,
     },
 });
