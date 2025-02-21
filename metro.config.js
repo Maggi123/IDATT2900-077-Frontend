664a9e7db5ddc8ae201ebec9346412e347e8902e
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-02-21 20:13:40 +0100

    Fix code style in Metro config

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from "expo/metro-config";
+/* eslint-env node */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
