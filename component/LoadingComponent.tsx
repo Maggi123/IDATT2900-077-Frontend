ed36fbc9c2be76ad602af4b7847b3c53c2beed89
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-02-21 20:37:09 +0100

    Create a component showing a loading screen

---
+++ component/LoadingComponent.tsx
@@ -0,0 +1,21 @@
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+
+import { Colors } from "@/constants/Colors";
+
+export default function LoadingComponent() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color={Colors.text} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.background,
+    gap: 20,
+  },
+});
