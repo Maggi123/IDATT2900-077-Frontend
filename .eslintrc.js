b97def9cae5848f796a8d968b3ee5607059f5c32
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-02-21 10:56:47 +0100

    Add TypeScript linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  extends: ["expo", "prettier", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["prettier", "@typescript-eslint"],
   rules: {
     "prettier/prettier": "error",
   },
