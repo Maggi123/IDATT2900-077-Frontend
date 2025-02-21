2074ab56d354d187695ef3ae31b30e06a2489ee5
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-02-21 20:13:19 +0100

    Add some more linting configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: [
     "expo",
     "prettier",
+    "universe/native",
+    "universe/shared/typescript-analysis",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
   ],
@@ -12,4 +14,12 @@ module.exports = {
     "prettier/prettier": "error",
   },
   ignorePatterns: ["/dist/*"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.d.ts"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  ],
 };
