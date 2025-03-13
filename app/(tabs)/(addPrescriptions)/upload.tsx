6a9c8c9d64337ba468b82b8df9d6dc708d0fa765
Gia Hy Nguyen <giahy03@outlook.com>
2025-03-13 09:15:38 +0100

    Add empty screens

---
+++ app/(tabs)/(addPrescriptions)/upload.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Upload() {
+  return (
+    <View>
+      <Text>File Upload Screen</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/(addPrescriptions)/url.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function URL() {
+  return (
+    <View>
+      <Text>URL Input Screen</Text>
+      {/* Implement URL input logic here */}
+    </View>
+  );
+}
