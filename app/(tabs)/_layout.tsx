5fcd91147ab53b0d95efd3d18ec12d24702e48f1
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-03-13 10:15:18 +0100

    Move Auth0Provider to root layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,11 +5,9 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router"; // Import for navigation
 import { useEffect, useState } from "react";
 import { TouchableOpacity, StyleSheet, View } from "react-native";
-import { Auth0Provider } from "react-native-auth0";
 
 import { initializeAgent } from "@/agent/Agent";
 import LoadingComponent from "@/component/LoadingComponent";
-import { Auth0Config } from "@/constants/Auth0Config";
 import { Colors } from "@/constants/Colors";
 import { getDidForAgent } from "@/util/DidUtil";
 
@@ -30,56 +28,54 @@ export default function TabLayout() {
   if (!agent) return <LoadingComponent />;
 
   return (
-    <Auth0Provider domain={Auth0Config.domain} clientId={Auth0Config.clientId}>
-      <AgentProvider agent={agent}>
-        <Tabs
-          screenOptions={{
-            headerShown: true,
-            headerStyle: {
-              backgroundColor: Colors.bar,
-            },
-            headerTintColor: Colors.lightpink,
-            headerTitleAlign: "center",
-            tabBarStyle: {
-              backgroundColor: Colors.bar,
-              height: 70,
-            },
-            tabBarShowLabel: false,
+    <AgentProvider agent={agent}>
+      <Tabs
+        screenOptions={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: Colors.bar,
+          },
+          headerTintColor: Colors.lightpink,
+          headerTitleAlign: "center",
+          tabBarStyle: {
+            backgroundColor: Colors.bar,
+            height: 70,
+          },
+          tabBarShowLabel: false,
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            tabBarButton: (props) => (
+              <TouchableOpacity {...props} style={styles.tabBarContainer}>
+                <View style={styles.circle}>
+                  <MaterialCommunityIcons
+                    name="home"
+                    size={35}
+                    color={Colors.bar}
+                  />
+                </View>
+              </TouchableOpacity>
+            ),
           }}
-        >
-          <Tabs.Screen
-            name="home"
-            options={{
-              tabBarButton: (props) => (
-                <TouchableOpacity {...props} style={styles.tabBarContainer}>
-                  <View style={styles.circle}>
-                    <MaterialCommunityIcons
-                      name="home"
-                      size={35}
-                      color={Colors.bar}
-                    />
-                  </View>
-                </TouchableOpacity>
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="(addPrescriptions)"
-            options={{
-              href: null,
-              headerShown: false,
-            }}
-          />
-          <Tabs.Screen
-            name="(viewPrescriptions)"
-            options={{
-              href: null,
-              headerShown: false,
-            }}
-          />
-        </Tabs>
-      </AgentProvider>
-    </Auth0Provider>
+        />
+        <Tabs.Screen
+          name="(addPrescriptions)"
+          options={{
+            href: null,
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="(viewPrescriptions)"
+          options={{
+            href: null,
+            headerShown: false,
+          }}
+        />
+      </Tabs>
+    </AgentProvider>
   );
 }
 
