51f0d378ba339ea72904b213d9856e9ccfef5a60
Magnus Gjerlaugsen <maggj@stud.ntnu.no>
2025-03-13 14:44:27 +0100

    Use default stylesheet in register.tsx

--- app/register.tsx
+++ app/register.tsx
@@ -1,25 +1,11 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
-import { Colors } from "@/constants/Colors"; // Make sure Colors are defined
+import { defaultStyles } from "@/stylesheets/defaultStyles";
 
 export default function Register() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Register</Text>
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.title}>Register</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: Colors.background,
-  },
-  title: {
-    fontSize: 40,
-    paddingTop: 30,
-    color: Colors.text,
-    fontWeight: "bold",
-  },
-});
