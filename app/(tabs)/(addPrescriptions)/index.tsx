bfa6f20d06b248ae20a656a2b2407ebb3efd1e9f
Gia Hy Nguyen <giahy03@outlook.com>
2025-04-09 09:34:19 +0200

    Update screen to use component for option button

--- app/(tabs)/(addPrescriptions)/index.tsx
+++ app/(tabs)/(addPrescriptions)/index.tsx
@@ -1,99 +1,37 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { View, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
-import { Colors } from "@/constants/Colors";
+import { defaultStyles } from "@/stylesheets/DefaultStyles";
+import NavigationOptionButton from "@/components/NavigationOptionButton";
 
 export default function AddPrescriptions() {
   const router = useRouter();
 
-  const handleButtonPress = (button: string) => {
-    if (button === "Button 1") {
-      router.push("/QRCodeScreen");
-    } else if (button === "Button 2") {
-      router.push("/URLScreen");
-    } else if (button === "Button 3") {
-      router.push("/UploadScreen");
-    }
-  };
-
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          handleButtonPress("Button 1");
-        }}
-      >
-        <View style={styles.buttonContent}>
-          <MaterialCommunityIcons
-            name="qrcode-scan"
-            size={24}
-            color={Colors.lightpurple}
-          />
-          <Text style={styles.buttonText}>Scan QR code</Text>
-        </View>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          handleButtonPress("Button 2");
-        }}
-      >
-        <View style={styles.buttonContent}>
-          <MaterialCommunityIcons
-            name="pencil-outline"
-            size={24}
-            color={Colors.lightpurple}
-          />
-          <Text style={styles.buttonText}>Input URL</Text>
-        </View>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          handleButtonPress("Button 3");
-        }}
-      >
-        <View style={styles.buttonContent}>
-          <MaterialCommunityIcons
-            name="upload-outline"
-            size={24}
-            color={Colors.lightpurple}
-          />
-          <Text style={styles.buttonText}>Upload document</Text>
-        </View>
-      </TouchableOpacity>
+      <NavigationOptionButton
+        icon="qrcode-scan"
+        text="Scan QR code"
+        onPress={() => router.push("/QRCodeScreen")}
+      />
+      <NavigationOptionButton
+        icon="pencil-outline"
+        text="Input URL"
+        onPress={() => router.push("/URLScreen")}
+      />
+      <NavigationOptionButton
+        icon="upload-outline"
+        text="Upload document"
+        onPress={() => router.push("/UploadScreen")}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    ...defaultStyles.container,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: Colors.background,
     gap: 20,
-  },
-  button: {
-    width: "90%",
-    backgroundColor: Colors.lightpink,
-    paddingVertical: 12,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonContent: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 10,
-  },
-  buttonText: {
-    color: Colors.lightpurple,
-    fontSize: 20,
   },
 });
