bec270651c0c59c44d39e10a18b6966b08b25cf5
Gia Hy Nguyen <giahy03@outlook.com>
2025-04-09 09:25:49 +0200

    Make prescription list into a component

---
+++ components/PrescriptionList.tsx
@@ -0,0 +1,128 @@
+import { W3cCredentialSubject } from "@credo-ts/core";
+import { Text, View, StyleSheet, SectionList, Pressable } from "react-native";
+
+import { Colors } from "@/constants/Colors";
+
+const PrescriptionList = ({
+                            prescriptions,
+                            issuerNames,
+                            selectedPrescriptions,
+                            onToggleSelection,
+                          }) => {
+  return (
+    <SectionList
+      style={styles.list}
+      sections={prescriptions.map((credential) => {
+        const data: W3cCredentialSubject[] = [];
+        if (credential.credential.credentialSubject instanceof Array) {
+          data.concat(credential.credential.credentialSubject);
+        } else {
+          data.push(credential.credential.credentialSubject);
+        }
+        return {
+          title: credential,
+          data,
+        };
+      })}
+      keyExtractor={(_, key) => `${key}`}
+      renderItem={({ section, item }) => (
+        <View style={styles.prescriptionBox}>
+          <View style={styles.topRow}>
+            <Text style={styles.issuer}>
+              {issuerNames[section.title.credential.issuerId] ??
+                section.title.credential.issuerId}
+            </Text>
+            <Pressable
+              onPress={() => onToggleSelection(JSON.stringify(item))}
+              style={[
+                styles.checkbox,
+                selectedPrescriptions.includes(JSON.stringify(item)) &&
+                styles.checked,
+              ]}
+              accessibilityRole="checkbox"
+            />
+          </View>
+          <Text style={styles.name}>
+            {item.claims ? (item.claims.name) : "N/A"}
+          </Text>
+          <View style={styles.detailRow}>
+            <Text style={styles.detailTitle}>Active ingredient: </Text>
+            <Text style={styles.detail}>
+              {item.claims
+                ? (item.claims.activeIngredient ?? "N/A")
+                : "N/A"}
+            </Text>
+          </View>
+          <View style={styles.detailRow}>
+            <Text style={styles.detailTitle}>Authored: </Text>
+            <Text style={styles.detail}>
+              {item.claims
+                ? new Date(item.claims.authoredOn).toLocaleDateString()
+                : "N/A"}
+            </Text>
+          </View>
+          <View style={styles.detailRow}>
+            <Text style={styles.detailTitle}>Added: </Text>
+            <Text style={styles.detail}>
+              {section.title.createdAt.toLocaleDateString()}
+            </Text>
+          </View>
+        </View>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  list: {
+    marginTop: 20,
+    width: "90%",
+    color: Colors.text,
+  },
+  prescriptionBox: {
+    backgroundColor: Colors.white,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: Colors.lightgray,
+    marginBottom: 10,
+  },
+  topRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 4,
+  },
+  issuer: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: Colors.darkpurple,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  detailRow: {
+    flexDirection: "row",
+    marginBottom: 4,
+  },
+  detailTitle: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  detail: {
+    fontSize: 14,
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: Colors.text,
+  },
+  checked: {
+    backgroundColor: Colors.text,
+  },
+});
+
+export default PrescriptionList;
